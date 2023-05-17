5b480908e3d2ec714e365fe33954305732662841
LeraN <133788550+Pangleran@users.noreply.github.com>
2023-05-17 12:50:26 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  token: process.env.token,
+  channel: "channel_tujuan", // Channel_ID
+  status: "idle", // online, idle, dnd, invisble
+  webhook: process.env.webhook // Webhook_ID
+} 
